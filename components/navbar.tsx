84c522ffcf4ca0d583d65c01829bdecc00fc17c8
trishanu-init <trishanu.nayak@outlook.com>
2024-02-23 20:23:44 +0530

    updated navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = async () => {
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-            <p className="font-bold text-xl">InieKart</p>
+            <p className="font-bold text-xl">IndieKart</p>
           </Link>
           <MainNav data={categories} />
           <NavbarActions />
