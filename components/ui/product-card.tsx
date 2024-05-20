0b72485ae1b8fb372de545591a1619f24a107de2
Ayushmaanagarwal1121 <agarwalayushmaan88@gmail.com>
2024-05-20 20:34:49 +0530

    Added hover effect when hovering on a product card .

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -39,7 +39,7 @@ const ProductCard: React.FC<ProductCard> = ({
   };
   
   return ( 
-    <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+    <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl hover-zoom transform hover:scale-105 transition-transform duration-[200] border p-3 space-y-4">
       {/* Image & actions */}
       <div className="aspect-square rounded-xl bg-gray-100 relative">
         <Image 
