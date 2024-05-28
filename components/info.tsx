48f8134d86b3382cb22f313e97b81a23ca8a98e9
Utsavladia <utsavladia@gmail.com>
2024-05-28 10:22:42 +0530

    seleting size option added

--- components/info.tsx
+++ components/info.tsx
@@ -2,23 +2,27 @@
 
 import { ShoppingCart } from "lucide-react";
 
-import Currency  from "@/components/ui/currency";
+import Currency from "@/components/ui/currency";
 import Button from "@/components/ui/button";
 import { Product } from "@/types";
 import useCart from "@/hooks/use-cart";
 
 interface InfoProps {
-  data: Product
-};
+  data: Product;
+}
 
 const Info: React.FC<InfoProps> = ({ data }) => {
   const cart = useCart();
 
   const onAddToCart = () => {
     cart.addItem(data);
-  }
+  };
 
-  return ( 
+  const sizes = {
+    value: ["S", "M", "L"],
+  };
+
+  return (
     <div>
       <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
       <div className="mt-3 flex items-end justify-between">
@@ -30,13 +34,22 @@ const Info: React.FC<InfoProps> = ({ data }) => {
       <div className="flex flex-col gap-y-6">
         <div className="flex items-center gap-x-4">
           <h3 className="font-semibold text-black">Size:</h3>
-          <div>
-            {data?.size?.value}
+          <div className=" font-sans font-semibold">
+            <select className="p-1  border-black border-2">
+              {sizes?.value.map((s, index) => (
+                <option key={index} value={s} className="font-semibold">
+                  {s}
+                </option>
+              ))}
+            </select>
           </div>
         </div>
         <div className="flex items-center gap-x-4">
           <h3 className="font-semibold text-black">Color:</h3>
-          <div className="h-6 w-6 rounded-full border border-gray-600" style={{ backgroundColor: data?.color?.value }} />
+          <div
+            className="h-6 w-6 rounded-full border border-gray-600"
+            style={{ backgroundColor: data?.color?.value }}
+          />
         </div>
       </div>
       <div className="mt-10 flex items-center gap-x-3">
@@ -47,6 +60,6 @@ const Info: React.FC<InfoProps> = ({ data }) => {
       </div>
     </div>
   );
-}
- 
+};
+
 export default Info;
