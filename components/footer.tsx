4ffc7c46b8fbc1f8fda61124f54e597e67386623
Aastha Kourav <aasthakourav2002@gmail.com>
2024-05-31 17:22:10 +0530

    enhanced footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,20 @@
-const Footer = () => {
+import React from 'react';
+import Link from "next/link";
+ const Footer = () => {
   return (
     <footer className="bg-white border-t">
-      <div className="mx-auto py-10">
+      <div className="max-auto py-10">
+      <div className="footer-links  flex flex-row justify-between items-center  
+              px-10 sm:px-6 lg:px-20">
+          <Link href="/">Home</Link>
+          <Link href="/About_us">About Us</Link>
+          <Link href="/Services">Services</Link>
+          <Link href="/Contact">Contact</Link>
+          <Link href="/Blog">Blogs</Link>
+          <Link href="/Blog">FAQs</Link>
+         
+       
+        </div><br></br>
         <p className="text-center text-xs text-black">
           &copy; {new Date().getFullYear()} Store, Inc. All rights reserved.
         </p>
