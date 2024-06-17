30468d005d4c0bf9db63908ea639b9d057be27d5
Jayanth <jayanththalla33@gmail.com>
2024-06-17 11:09:57 +0530

    add logo
    i added logo to the title

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,13 +8,16 @@ import Navbar from '@/components/navbar'
 // import HomePage from './(routes)/page'
 import Footer from '@/components/footer'
 import AnimatedCursor from 'react-animated-cursor'
+
 import './globals.css'
+
 
 const font = Urbanist({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'IndieKart',
   description: 'IndieKart - The place for all your purchases.',
+  logo: '../public/logo.svg', // Update the logo path he
 }
 
 export default function RootLayout({
@@ -24,42 +27,40 @@ export default function RootLayout({
 }) {
   return (
     <>
-    
       
-    <html lang="en">
-      <body className={font.className}>
-      <AnimatedCursor 
-        innerSize={9}
-        outerSize={40}
-        color="98, 111, 152"
-        outerAlpha={.2}
-        innerScale={0.7}
-        outerScale={3}
-        clickables={[
+      <html lang="en">
+        <body className={font.className}>
+          <AnimatedCursor 
+            innerSize={9}
+            outerSize={40}
+            color="98, 111, 152"
+            outerAlpha={.2}
+            innerScale={0.7}
+            outerScale={3}
+            clickables={[
+              'a',
+              'input[type="text"]',
+              'input[type="email"]',
+              'input[type="number"]',
+              'input[type="submit"]',
+              'input[type="image"]',
+              'label[for]',
+              'select',
+              'textarea',
+              'button',
+              '.link'
+            ]}
+          />
+          <ToastProvider />
           
-          'a',
-          'input[type="text"]',
-          'input[type="email"]',
-          'input[type="number"]',
-          'input[type="submit"]',
-          'input[type="image"]',
-          'label[for]',
-          'select',
-          'textarea',
-          'button',
-          '.link'
-        ]}
-    />
-        <ToastProvider />
-        
-        <ModalProvider />
-        <Navbar />
+          <ModalProvider />
+          <Navbar />
 
-        {children}
-        <SpeedInsights /> 
-        <Footer />
-      </body>
-    </html>
+          {children}
+          <SpeedInsights /> 
+          <Footer />
+        </body>
+      </html>
     </>
   )
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,18 +1,23 @@
 import Link from "next/link";
-
 import MainNav from "@/components/main-nav";
 import Container from "@/components/ui/container";
 import NavbarActions from "@/components/navbar-actions";
 import getCategories from "@/actions/get-categories";
+import Image from 'next/image'; // Import Image component from next/image
 
 const Navbar = async () => {
   const categories = await getCategories();
 
   return (
     <div className="border-b fixed top-0 left-0 z-50 bg-white w-full overflow-auto">
-
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+          <Image
+            src="/logo.svg" // Path to your logo image
+            alt="IndieKart Logo"
+            width={50} // Adjust width as per your design
+            height={2} // Adjust height as per your design
+          />
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
             <p className="font-bold text-xl">IndieKart</p>
           </Link>
