42db8887ccdc3cd4592bae694269b43a3654599f
Utsavladia <utsavladia@gmail.com>
2024-06-25 13:55:53 +0530

    added functionality of wishlist

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ShoppingBag } from "lucide-react";
+import { ShoppingBag, Heart } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -21,19 +21,22 @@ const NavbarActions = () => {
     return null;
   }
 
-  return ( 
-    <div className="ml-auto flex items-center gap-x-4">
-      <Button onClick={() => router.push('/cart')} className="flex items-center rounded-full bg-black px-4 py-2">
-        <ShoppingBag
-          size={20}
-          color="white"
-        />
+  return (
+    <div className="ml-auto flex items-center md:gap-x-4 gap-x-1">
+      <Button
+        onClick={() => router.push("/cart")}
+        className="flex items-center rounded-full bg-black md:px-4 px-1 py-1 md:py-2"
+      >
+        <ShoppingBag size={20} color="white" />
         <span className="ml-2 text-sm font-medium text-white">
           {cart.items.length}
         </span>
       </Button>
+      <div onClick={() => router.push("/wishlist")}>
+        <Heart />
+      </div>
     </div>
   );
-}
- 
+};
+
 export default NavbarActions;
