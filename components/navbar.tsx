3d36bc888c30c982895e22031dfcc9f244d91844
Jayanth <jayanththalla33@gmail.com>
2024-06-25 09:26:51 +0530

    logo is added

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -11,8 +11,9 @@ const Navbar = async () => {
     <div className="border-b fixed top-0 left-0 z-50 bg-white w-full overflow-auto">
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center justify-between">
-          <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-            <p className="font-bold text-xl">IndieKart</p>
+        <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2 items-center">
+          <img src="/indiekart_logo.png" alt="logo" className="h-10" />
+          <p className="font-bold text-xl">IndieKart</p>
           </Link>
           <MainNav data={categories} />
           <div className="flex items-center gap-x-4">
